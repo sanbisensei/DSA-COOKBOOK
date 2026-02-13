a9517950b50133613665ebfaad9f971274f6bca9
sanbisensei <lsunbi1@gmail.com>
2026-02-13 22:14:48 +0600

    BASIC MATH

--- BASIC MATH/DIVISORS/CountFactors.cpp
+++ BASIC MATH/DIVISORS/CountFactors.cpp
@@ -6,16 +6,12 @@ int main(){
     int n;
     cin>>n;
     int count=0;
-    for(int i=1;i*i<=n;i++){
+    for(int i=1;i<=n;i++){
         if(n%i == 0){
-            count++;
-
-            if(i != n/i){
-            count++;
-        }
+           count++;
         }
     }
-    cout<<count<<endl;
+        cout<< count <<endl;
 
     
     return 0;
--- BASIC MATH/DIVISORS/PrintFactorsFaster.cpp
+++ BASIC MATH/DIVISORS/PrintFactorsFaster.cpp
@@ -5,17 +5,16 @@ int main(){
 
     int n;
     cin>>n;
-    int count=0;
+   
     for(int i=1;i*i<=n;i++){
         if(n%i == 0){
-            count++;
+            cout << i << " ";
 
             if(i != n/i){
-            count++;
+            cout << n/i << " ";
         }
         }
     }
-    cout<<count<<endl;
 
     
     return 0;
