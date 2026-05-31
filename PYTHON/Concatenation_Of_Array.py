b4618729364efccc23f97d5c15c9177cf9d1b17f
sanbisensei <lsunbi1@gmail.com>
2026-05-31 19:43:25 +0600

    leetcode

---
+++ PYTHON/Concatenation_Of_Array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        new_nums = nums
+        for i in range(0,len(nums),1):
+            new_nums.append(nums[i])
+        return new_nums
+
+
+nums = [1, 2, 3, 4, 5]
+
+sol = Solution()
+print(sol.getConcatenation(nums))
+
+
+
+
+# alternate solve:
+# class Solution(object):
+#     def getConcatenation(self, nums):
+#         return nums + nums
--- STL/Pairs.cpp
+++ STL/Pairs.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main(){
-    
+    pair<int,string> p;
+    // p = make_pair(2,"abc");
+    p={2,"abul"};
+    pair<int,string> p1=p;
+    p1.first=69;
+    cout<<p1.first;
+    cout<<p1.second;
     return 0;
 }
